5ee1e45b50d3a028117eff1d29ca3e34900a2948
Minhajul Malek <minhajul.malek@ad.infosys.com>
2018-08-15 13:16:59 -0400

    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FlightComponent } from './flight/flight.component';
 import { FlightDetailsComponent } from './flight-details/flight-details.component';
+import { IdPipe } from './id.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FlightComponent,
-    FlightDetailsComponent
+    FlightDetailsComponent,
+    IdPipe
   ],
   imports: [
     BrowserModule
